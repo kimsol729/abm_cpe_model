6ae0187484e243caa7bfb053c9e1193f00d4852c
Kim Sol <93749644+kimsol729@users.noreply.github.com>
2025-11-10 17:30:48 +0900

    intervention simul final

--- src/intervention_month.py
+++ src/intervention_month.py
@@ -15,218 +15,16 @@ from tqdm import tqdm
 # %%
 
 # Step 1. Data type 정하기
-data_type = 'A'
-if data_type == 'A':
-    runtime = 30*19
-    df = pd.read_csv("../emulation_calibration/calibration_results_A.csv")
-
-elif data_type == 'B':
-    runtime = 30*36
-    df = pd.read_csv("../emulation_calibration/calibration_results_B.csv")
-
-np.random.seed(4321)
-samples = np.random.choice(df["x"], 100, replace=True) # of Samples = 50
-probTransmission_samples = samples
-
-plt.figure(figsize=(12, 5))
-plt.subplot(1, 2, 1)
-plt.hist(df["x"], bins=30, edgecolor="black", alpha=0.7, label="Original")
-plt.xlabel("x values")
-plt.ylabel("Frequency")
-plt.title("Original Distribution")
-plt.legend()
-x_min, x_max = df["x"].min(), df["x"].max()
-plt.xlim(x_min, x_max)
-plt.subplot(1, 2, 2)
-plt.hist(samples, bins=30, edgecolor="black", alpha=0.7, label="Sampled")
-plt.xlabel("x values")
-plt.ylabel("Frequency")
-plt.title("Sampled Distribution")
-plt.legend()
-plt.xlim(x_min, x_max)
-plt.tight_layout()
-plt.show()
-
-#%% Step2. 어떤 변수를 몇으로 할건지 정하기
-
-variable_name = 'hcw_wash_rate'
-variable_value = [0.80, 0.90, 0.95, 0.99]
-
-# variable_name = 'cleaningDay'
-# variable_value = [30, 60, 90, 180]
-
-# variable_name = 'isolation_time'
-# variable_value = [3, 7, 10, 14]
-for i in variable_value:
-    probNewPatient = 0.003
-    isolationFactor = 0.75
-    height=11
-    width=32
-
-    fixed_params = {
-        "data_type" : data_type , 
-        "prob_new_patient" : probNewPatient, 
-        "isolation_factor" : isolationFactor, 
-        "cleaningDay" : 180,
-        "hcw_wash_rate" : 0.9, 
-        "isolation_time" : 14, 
-        "height" : height, "width" : width 
-        }
-    del fixed_params[variable_name]
-    fixed_params.update({variable_name: i})
-
-    model = CPE_Model_month(
-        data_type=data_type,
-        prob_new_patient=probNewPatient, 
-        prob_transmission=000, 
-        isolation_factor=isolationFactor,
-        cleaningDay=180,
-        hcw_wash_rate=0.9,
-        isolation_time=14,
-        height=height, width=width
-        )
-    variable_params = {'prob_transmission' : probTransmission_samples}
-    batch_run = BatchRunner(
-    CPE_Model_month,
-    variable_parameters = variable_params,
-    fixed_parameters = fixed_params,
-    iterations = 1,
-    max_steps = model.ticks_in_day * runtime,
-    display_progress=True,
-    model_reporters={"HCW_related_infecs" : getTotalInfec}
-    )
-    print('now run')
-    batch_run.run_all()
-    run_data = batch_run.get_model_vars_dataframe()
-    
-
-    csv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..',
-        'result/intervention_test_wash2_{}.csv'.format(i))
-    run_data.to_csv(csv_path)
-
-    print("done!!")
-
-
-# %%
-
-variable_name = 'cleaningDay'
-variable_value = [30, 60, 90, 180]
-
-for i in variable_value:
-    start_time = time.time()
-    probNewPatient = 0.003
-    isolationFactor = 0.75
-    height=11
-    width=32
-
-    fixed_params = {
-        "data_type" : data_type , 
-        "prob_new_patient" : probNewPatient, 
-        "isolation_factor" : isolationFactor, 
-        "cleaningDay" : 180,
-        "hcw_wash_rate" : 0.9, 
-        "isolation_time" : 14, 
-        "height" : height, "width" : width 
-        }
-    del fixed_params[variable_name]
-    fixed_params.update({variable_name: i})
-
-    model = CPE_Model_month(
-        data_type=data_type,
-        prob_new_patient=probNewPatient, 
-        prob_transmission=000, 
-        isolation_factor=isolationFactor,
-        cleaningDay=180,
-        hcw_wash_rate=0.9,
-        isolation_time=14,
-        height=height, width=width
-        )
-    variable_params = {'prob_transmission' : probTransmission_samples}
-    batch_run = BatchRunner(
-    CPE_Model_month,
-    variable_parameters = variable_params,
-    fixed_parameters = fixed_params,
-    iterations = 1,
-    max_steps = model.ticks_in_day * runtime,
-    display_progress=True,
-    model_reporters={"HCW_related_infecs" : getTotalInfec}
-    )
-    print('now run')
-    batch_run.run_all()
-    run_data = batch_run.get_model_vars_dataframe()
-    
-
-    csv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..',
-        'result/intervention_test_clean2_{}.csv'.format(i))
-    run_data.to_csv(csv_path)
-
-    print("done!!")
-
-    #%%
-
-variable_name = 'isolation_time'
-variable_value = [3, 7, 10, 14]
-for i in variable_value:
-    start_time = time.time()
-    probNewPatient = 0.003
-    isolationFactor = 0.75
-    height=11
-    width=32
-
-    fixed_params = {
-        "data_type" : data_type , 
-        "prob_new_patient" : probNewPatient, 
-        "isolation_factor" : isolationFactor, 
-        "cleaningDay" : 180,
-        "hcw_wash_rate" : 0.9, 
-        "isolation_time" : 14, 
-        "height" : height, "width" : width 
-        }
-    del fixed_params[variable_name]
-    fixed_params.update({variable_name: i})
-
-    model = CPE_Model_month(
-        data_type=data_type,
-        prob_new_patient=probNewPatient, 
-        prob_transmission=000, 
-        isolation_factor=isolationFactor,
-        cleaningDay=180,
-        hcw_wash_rate=0.9,
-        isolation_time=14,
-        height=height, width=width
-        )
-    variable_params = {'prob_transmission' : probTransmission_samples}
-    batch_run = BatchRunner(
-    CPE_Model_month,
-    variable_parameters = variable_params,
-    fixed_parameters = fixed_params,
-    iterations = 1,
-    max_steps = model.ticks_in_day * runtime,
-    display_progress=True,
-    model_reporters={"HCW_related_infecs" : getTotalInfec}
-    )
-    print('now run')
-    batch_run.run_all()
-    run_data = batch_run.get_model_vars_dataframe()
-    
-
-    csv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..',
-        'result/intervention_test_iso2_{}.csv'.format(i))
-    run_data.to_csv(csv_path)
-
-    print("done!!")
-
-# %%
-
-# Step 1. Data type 정하기
 data_type = 'B'
 if data_type == 'A':
     runtime = 30*19
-    df = pd.read_csv("../emulation_calibration/calibration_results_A.csv")
+    df = pd.read_csv("../emulation_calibration/B_samp_A.csv")
+    df = df[2000:]
 
 elif data_type == 'B':
     runtime = 30*36
-    df = pd.read_csv("../emulation_calibration/calibration_results_B.csv")
+    df = pd.read_csv("../emulation_calibration/B_samp_B.csv")
+    df = df[2000:]
 
 np.random.seed(4321)
 samples = np.random.choice(df["x"], 50, replace=True) # of Samples = 50
@@ -253,77 +51,15 @@ plt.show()
 
 #%% Step2. 어떤 변수를 몇으로 할건지 정하기
 
-variable_name = 'hcw_wash_rate'
-variable_value = [0.80, 0.90, 0.95, 0.99]
-
-# variable_name = 'cleaningDay'
-# variable_value = [30, 60, 90, 180]
-
-# variable_name = 'isolation_time'
-# variable_value = [3, 7, 10, 14]
-for i in variable_value:
-    start_time = time.time()
-    probNewPatient = 0.003
-    isolationFactor = 0.75
-    height=11
-    width=32
-
-    fixed_params = {
-        "data_type" : data_type , 
-        "prob_new_patient" : probNewPatient, 
-        "isolation_factor" : isolationFactor, 
-        "cleaningDay" : 180,
-        "hcw_wash_rate" : 0.9, 
-        "isolation_time" : 14, 
-        "height" : height, "width" : width 
-        }
-    del fixed_params[variable_name]
-    fixed_params.update({variable_name: i})
-
-    model = CPE_Model_month(
-        data_type=data_type,
-        prob_new_patient=probNewPatient, 
-        prob_transmission=000, 
-        isolation_factor=isolationFactor,
-        cleaningDay=180,
-        hcw_wash_rate=0.9,
-        isolation_time=14,
-        height=height, width=width
-        )
-    variable_params = {'prob_transmission' : probTransmission_samples}
-    batch_run = BatchRunner(
-    CPE_Model_month,
-    variable_parameters = variable_params,
-    fixed_parameters = fixed_params,
-    iterations = 1,
-    max_steps = model.ticks_in_day * runtime,
-    display_progress=True,
-    model_reporters={"HCW_related_infecs" : getTotalInfec}
-    )
-    print('now run')
-    batch_run.run_all()
-    run_data = batch_run.get_model_vars_dataframe()
-    
-
-    csv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..',
-        'result/intervention_test_wash2B_{}.csv'.format(i))
-    run_data.to_csv(csv_path)
-
-    print("done!!")
-
-
-# %%
-
 # variable_name = 'hcw_wash_rate'
-# variable_value = [0.80, 0.90, 0.99]
+# variable_value = [0.95]
 
 variable_name = 'cleaningDay'
 variable_value = [30, 60, 90, 180]
 
 # variable_name = 'isolation_time'
-# variable_value = [3, 7, 10, 14]
+# variable_value = [14]
 for i in variable_value:
-    start_time = time.time()
     probNewPatient = 0.003
     isolationFactor = 0.75
     height=11
@@ -356,7 +92,7 @@ for i in variable_value:
     CPE_Model_month,
     variable_parameters = variable_params,
     fixed_parameters = fixed_params,
-    iterations = 1,
+    iterations = 50,
     max_steps = model.ticks_in_day * runtime,
     display_progress=True,
     model_reporters={"HCW_related_infecs" : getTotalInfec}
@@ -367,14 +103,69 @@ for i in variable_value:
     
 
     csv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..',
-        'result/intervention_test_clean2B{}.csv'.format(i))
+        'result/new_intervention_test_cleanB_{}.csv'.format(i))
     run_data.to_csv(csv_path)
 
     print("done!!")
-variable_name = 'isolation_time'
-variable_value = [3, 7, 10, 14]
+
+
+   
+plt.hist(samples, bins=30, edgecolor="black", alpha=0.7, label="Sampled")
+plt.xlabel("x values")
+plt.ylabel("Frequency")
+plt.title("Sampled Distribution")
+plt.legend()
+plt.xlim(x_min, x_max)
+plt.tight_layout()
+plt.show()
+
+
+# Step 1. Data type 정하기
+data_type = 'A'
+if data_type == 'A':
+    runtime = 30*19
+    df = pd.read_csv("../emulation_calibration/B_samp_A.csv")
+    df = df[2000:]
+
+elif data_type == 'B':
+    runtime = 30*36
+    df = pd.read_csv("../emulation_calibration/B_samp_B.csv")
+    df = df[2000:]
+
+np.random.seed(4321)
+samples = np.random.choice(df["x"], 50, replace=True) # of Samples = 50
+probTransmission_samples = samples
+
+plt.figure(figsize=(12, 5))
+plt.subplot(1, 2, 1)
+plt.hist(df["x"], bins=30, edgecolor="black", alpha=0.7, label="Original")
+plt.xlabel("x values")
+plt.ylabel("Frequency")
+plt.title("Original Distribution")
+plt.legend()
+x_min, x_max = df["x"].min(), df["x"].max()
+plt.xlim(x_min, x_max)
+plt.subplot(1, 2, 2)
+plt.hist(samples, bins=30, edgecolor="black", alpha=0.7, label="Sampled")
+plt.xlabel("x values")
+plt.ylabel("Frequency")
+plt.title("Sampled Distribution")
+plt.legend()
+plt.xlim(x_min, x_max)
+plt.tight_layout()
+plt.show()
+
+# Step2. 어떤 변수를 몇으로 할건지 정하기
+
+# variable_name = 'hcw_wash_rate'
+# variable_value = [0.95]
+
+variable_name = 'cleaningDay'
+variable_value = [30, 60, 90, 180]
+
+# variable_name = 'isolation_time'
+# variable_value = [14]
 for i in variable_value:
-    start_time = time.time()
     probNewPatient = 0.003
     isolationFactor = 0.75
     height=11
@@ -399,7 +190,7 @@ for i in variable_value:
         isolation_factor=isolationFactor,
         cleaningDay=180,
         hcw_wash_rate=0.9,
-        isolation_time=14,
+        isolation_time=14, 
         height=height, width=width
         )
     variable_params = {'prob_transmission' : probTransmission_samples}
@@ -407,7 +198,7 @@ for i in variable_value:
     CPE_Model_month,
     variable_parameters = variable_params,
     fixed_parameters = fixed_params,
-    iterations = 1,
+    iterations = 50,
     max_steps = model.ticks_in_day * runtime,
     display_progress=True,
     model_reporters={"HCW_related_infecs" : getTotalInfec}
@@ -418,8 +209,9 @@ for i in variable_value:
     
 
     csv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..',
-        'result/intervention_test_iso2B_{}.csv'.format(i))
+        'result/new_intervention_test_cleanA_{}.csv'.format(i))
     run_data.to_csv(csv_path)
 
     print("done!!")
+
 # %%
